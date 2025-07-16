0401543a4e41455c976f72bf100d70a9c3ffc252
Sérgio Nunes <sagnunes@gmail.com>
2025-07-16 12:12:07 +0100

    feat(security): enforce auth middleware for funds routes and optimize indexes
    Apply `auth` and `verified` middleware to "Coleção Digital" routes to secure access. Add database indexes to improve query performance on frequently used columns. Implement `MustVerifyEmail` for user model to ensure email verification.

--- database/migrations/2025_07_15_095625_create_digital_objects_table.php
+++ database/migrations/2025_07_15_095625_create_digital_objects_table.php
@@ -7,8 +7,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -24,7 +23,15 @@ return new class extends Migration
             $table->string('website_link')->nullable();
             $table->foreignIdFor(Fund::class)->constrained()->cascadeOnDelete()->cascadeOnUpdate();
             $table->foreignIdFor(App\Models\Status::class)->default(App\Enums\Status::UNPUBLISHED)->constrained()->cascadeOnUpdate();
+
+            $table->index('status_id', 'idx_status_id');
+
+            $table->index('image_thumb', 'idx_image_thumb');
+
+            $table->index(['fund_id', 'title', 'inventory_number'], 'idx_fund_title_inventory');
         });
+
+
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,10 @@ Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('colecao-digital', App\Http\Controllers\Funds\IndexFundController::class)->name('funds.index');
-Route::get('colecao-digital/{fund:acronym}', App\Http\Controllers\Funds\ShowDigitalObjectsByFundController::class)->name('funds.show');
+Route::group(['middleware' => ['auth', 'verified']], function () {
+    Route::get('colecao-digital', App\Http\Controllers\Funds\IndexFundController::class)->name('funds.index');
+    Route::get('colecao-digital/{fund:acronym}', App\Http\Controllers\Funds\ShowDigitalObjectsByFundController::class)->name('funds.show');
+});
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
